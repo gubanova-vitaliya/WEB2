6514e1707e348822bd25a328f92e898de5d4f6a2
guban <vitaliagubanova@gmail.com>
2025-12-10 11:40:52 +0300

    1

--- gase-frontend/src/App.tsx
+++ gase-frontend/src/App.tsx
@@ -3,8 +3,6 @@ import { AppNavbar } from "./components/Navbar";
 import { HomePage } from "./pages/HomePage";
 import { GasesPage } from "./pages/GasesPage";
 import { GasDetailPage } from "./pages/GasDetailPage";
-import { ReduxDemo } from "./components/ReduxDemo";
-import { NotesManager } from "./components/NotesManager";
 import { ROUTES } from "./Routes";
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
         <Route path={ROUTES.HOME} element={<HomePage />} />
         <Route path={ROUTES.GASES} element={<GasesPage />} />
         <Route path={`${ROUTES.GASES}/:id`} element={<GasDetailPage />} />
-        <Route path={ROUTES.REDUX_DEMO} element={<ReduxDemo />} />
-        <Route path={ROUTES.NOTES} element={<NotesManager />} />
         <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
       </Routes>
     </>
--- gase-frontend/src/Routes.tsx
+++ gase-frontend/src/Routes.tsx
@@ -1,8 +1,6 @@
 export const ROUTES = {
   HOME: "/",
   GASES: "/gases",
-  REDUX_DEMO: "/redux-demo",
-  NOTES: "/notes",
 };
 
 export type RouteKeyType = keyof typeof ROUTES;
@@ -10,8 +8,6 @@ export type RouteKeyType = keyof typeof ROUTES;
 export const ROUTE_LABELS: { [key in RouteKeyType]: string } = {
   HOME: "Главная",
   GASES: "Газы",
-  REDUX_DEMO: "Redux Demo",
-  NOTES: "Заметки",
 };
 
 
